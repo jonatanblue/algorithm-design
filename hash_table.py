695cf5c1085708cd10722c9f4a67b74a30e09101
Jonatan <jonatanblue@users.noreply.github.com>
2021-02-22 19:17:04 +0000

    Error if key > m
    Add helpful error message and comment

--- hash_table.py
+++ hash_table.py
@@ -3,7 +3,7 @@
 
 class HashTable:
     def __init__(self):
-        self.m = 113
+        self.m = 113  # Should be a prime number, larger than len(key)
         self.alphabet = "abcdefghijklmnopqrstuvwxyz"
         self.alpha = len(self.alphabet)
 
@@ -12,6 +12,12 @@ class HashTable:
 
     def make_hash(self, key: str):
         i = len(key) - 1
+        if i >= self.m:
+            raise ValueError(
+                f"The key length ({i}) is greater than m ({self.m}). "
+                "This will increase collisions and reduce performance. "
+                "Either pick a larger prime for m, or reduce the key length."
+            )
         product = (pow(self.alpha, self.m - (i + 1)) * self.char(key[0]))
         if i == 0:
             return product % self.m
